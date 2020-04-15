bcf1c72f10aa0a42241c9287d3dcaa7a8642299f
abhisaini <abhisni@iitk.ac.in>
2020-04-15 22:33:26 +0530

    basic

--- Exam_Portal/settings.py
+++ Exam_Portal/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'Exam_Portal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': 'ec2-34-234-228-127.compute-1.amazonaws.com',
+        'POST': '5432'
     }
 }
 
