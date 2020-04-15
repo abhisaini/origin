cd8226db20f10abb6a84f17eb53bc212cf52a220
abhisaini <abhisni@iitk.ac.in>
2020-04-15 16:17:26 +0530

    basic

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, render_to_response
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required, user_passes_test
 from app.forms import UserForm, LoginForm, PaperForm
