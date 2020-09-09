47bcbb4dc46e8872b5a932eca5401cde0cff98f0
Abhishek Saini <abhishek.saini@ETV-VLVCF.local>
2020-09-09 20:58:31 +0530

    ip

--- app/views.py
+++ app/views.py
@@ -27,11 +27,10 @@ Q_TYPE = {
     "I": "INTEGER",
     "C": "MULIPLE_CORRECT"
 }
-
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[-1].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
